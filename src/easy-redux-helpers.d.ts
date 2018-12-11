05fcca13e341cfd78fdaeb7c6a3844742a0fa0ef
Edwin Páez <edwin.paez@grability.com>
2018-12-11 18:38:07 -0500

    add typescript definition file

--- src/easy-redux-helpers.d.ts
+++ src/easy-redux-helpers.d.ts
@@ -1,23 +1,55 @@
+import { Reducer, AnyAction, ActionCreator, Action } from "redux";
+import { Selector } from "reselect";
+import { ComponentClass } from "../../../../Library/Caches/typescript/3.1/node_modules/@types/react";
 
+type State = any;
+// ***************
+// createReducer()
+// ***************
+/**
+ * A reducer with immer is a reducer that optionally returns state
+ */
+export type ReducerWithImmer<State, A extends Action = AnyAction> = (
+  state: State | undefined,
+  action: AnyAction
+) => State | undefined
 
-export type ReducerConfig = {
-  initialState: any,
-  [key: string]: function(state: any, action: any) : any
+export type SetOfReducers<S = State, A extends Action = AnyAction> = {
+  [key: string]: ReducerWithImmer<S, A>
 }
-export type createReducer = function(ReducerConfig) : function(state: object, action: any) : object
-
-
-
-export type PropsConfig = {
-  [key: string]: string
+/**
+ * The config of a reducer. It defines the initial state and a set of
+ * reducers of type ReducerWithImmer
+ */
+export type ReducerConfig<State, Action extends AnyAction> = SetOfReducers<State, Action> & {
+  initialState: State,
 }
-export type ActionsConfig = {
-  [key: string]: function() : object
+export type createReducer<S = State, A extends Action = AnyAction> = (config: ReducerConfig<S, A>) => Reducer
+
+
+// ***************
+// connect()
+// ***************
+/**
+ * An object in which each key is the name of the prop and each value is a string
+ * that specifies the part of the state that is mapped to it, for example:
+ * title:
+ */
+export interface PropsConfig<S,R> {
+  [key: string]: string | Selector<S,R>
 }
 
-export type ConnectConfig = {
-  props: (PropsConfig | function(state: object, props: object) : object)?,
-  actions: any?
+export interface ActionsConfig<AnyAction> {
+  [key: string]: ActionCreator<AnyAction>
 }
-export type connect = function(React.Component) : React.Component
+
+export interface ConnectConfig<S, R, A> {
+  props: PropsConfig<S, R>
+  actions: ActionsConfig<A>
+}
+
+export type ConfigurableConnect = {
+  to: (ConnectConfig) => ComponentClass
+}
+export type connect = (ComponentClass) => ConfigurableConnect
 
