0fee979d69538af0ce8871c61f1edd23769251b8
Edwin Páez <edwin.paez@grability.com>
2018-12-11 18:45:55 -0500

    add more comments to d.ts

--- src/easy-redux-helpers.d.ts
+++ src/easy-redux-helpers.d.ts
@@ -12,20 +12,24 @@ type State = any;
 export type ReducerWithImmer<State, A extends Action = AnyAction> = (
   state: State | undefined,
   action: AnyAction
-) => State | undefined
+) => State | undefined;
 
 export type SetOfReducers<S = State, A extends Action = AnyAction> = {
-  [key: string]: ReducerWithImmer<S, A>
-}
+  [key: string]: ReducerWithImmer<S, A>;
+};
 /**
  * The config of a reducer. It defines the initial state and a set of
  * reducers of type ReducerWithImmer
  */
-export type ReducerConfig<State, Action extends AnyAction> = SetOfReducers<State, Action> & {
-  initialState: State,
-}
-export type createReducer<S = State, A extends Action = AnyAction> = (config: ReducerConfig<S, A>) => Reducer
-
+export type ReducerConfig<State, Action extends AnyAction> = SetOfReducers<
+  State,
+  Action
+> & {
+  initialState: State;
+};
+export type createReducer<S = State, A extends Action = AnyAction> = (
+  config: ReducerConfig<S, A>
+) => Reducer;
 
 // ***************
 // connect()
@@ -33,23 +37,32 @@ export type createReducer<S = State, A extends Action = AnyAction> = (config: Re
 /**
  * An object in which each key is the name of the prop and each value is a string
  * that specifies the part of the state that is mapped to it, for example:
- * title:
+ * userName: "state.profile.name"
  */
-export interface PropsConfig<S,R> {
-  [key: string]: string | Selector<S,R>
+export interface PropsConfig<S, R> {
+  [key: string]: string | Selector<S, R>;
 }
-
+/**
+ * An object in which each key is the name of the action as the component will use it
+ * and the value is an action creator. All actions are mapped to the props.actions object
+ * in the component.
+ */
 export interface ActionsConfig<AnyAction> {
-  [key: string]: ActionCreator<AnyAction>
+  [key: string]: ActionCreator<AnyAction>;
 }
-
+/**
+ * A configuration object to connect the component to redux. Can have
+ * props property of type PropsConfig and an actions property of type ActionsConfig
+ */
 export interface ConnectConfig<S, R, A> {
-  props: PropsConfig<S, R>
-  actions: ActionsConfig<A>
+  props: PropsConfig<S, R> | undefined;
+  actions: ActionsConfig<A> | undefined;
 }
 
 export type ConfigurableConnect = {
-  to: (ConnectConfig) => ComponentClass
-}
-export type connect = (ComponentClass) => ConfigurableConnect
-
+  to: (ConnectConfig) => ComponentClass;
+};
+/**
+ * connects a component to redux by calling the `to` method with a ConnectConfig
+ */
+export type connect = (ComponentClass) => ConfigurableConnect;
